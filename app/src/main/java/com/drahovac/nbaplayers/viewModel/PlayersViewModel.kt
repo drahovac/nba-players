b29010e2e4f61d7747f8bebb1760bc39ae6b9cf7
Vaclav Drahorad <drahorad.vac@gmail.com>
2023-05-31 11:37:24 +0200

    Cache players list in viewmodel

--- app/src/main/java/com/drahovac/nbaplayers/viewModel/PlayersViewModel.kt
+++ app/src/main/java/com/drahovac/nbaplayers/viewModel/PlayersViewModel.kt
@@ -1,8 +1,13 @@
 package com.drahovac.nbaplayers.viewModel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.drahovac.nbaplayers.data.PlayersRepository
 
+/**
+ * View model for players screen, caches players list.
+ */
 class PlayersViewModel(repository: PlayersRepository) : ViewModel() {
-    val players = repository.getPlayers().flow
+    val players = repository.getPlayers().flow.cachedIn(viewModelScope)
 }
